122298bf7703ac11a68bc8ccf89630b6bf2603aa
mslimani <mohammed.slimani@hubone.fr>
2019-08-05 11:39:53 +0200

    màj

--- tests/ProductTest.class.php
+++ tests/ProductTest.class.php
@@ -3,10 +3,7 @@ require_once('../www/Product.class.php');
 use PHPUnit\Framework\TestCase;
 
 class ProductTest extends TestCase{
-    public function setUp(){}
-    public function tearDown(){}
 
-    
     public function testProductName(){
         $productName = 'carotte';
         $produit = new Product($productName,2,3.0);
