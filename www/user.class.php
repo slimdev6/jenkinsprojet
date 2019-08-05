d21e30d1559d5af24fe8c0075bb354c2f38325a8
mslimani <mohammed.slimani@hubone.fr>
2019-08-05 10:14:54 +0200

    màj

---
+++ www/user.class.php
@@ -0,0 +1,15 @@
+class User{
+    private $username;
+    private $role;
+
+
+
+    public function __construct($username,$role){
+        $this->username = $username;
+        $this->role = $role;
+    }
+
+    
+
+
+}
