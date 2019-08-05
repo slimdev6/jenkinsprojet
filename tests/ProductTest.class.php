094a1f073d9c6a25103033b515c40bd3bdbb7627
mslimani <mohammed.slimani@hubone.fr>
2019-08-05 14:52:13 +0200

    1

--- tests/ProductTest.class.php
+++ tests/ProductTest.class.php
@@ -8,7 +8,7 @@ class ProductTest extends TestCase{
         $productName = '0';
         $produit = new Product("ss",2,3.0);
         //$this->assertTrue($produit->getNom()!==false);
-        $this->assertTrue($produit->setNom(10) !==false);
+        $this->assertTrue($produit->setNom("10") !==false);
     }
 
 }
